1ec2ab5935d1cfcac75eef40ce5daabe47a27cd7
theodesp <theo.despoudis@wpengine.com>
2022-03-30 12:45:45 +0100

    refactor(banner image): update banner image altText

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Page() {
   const mainBanner = {
     sourceUrl: '/static/banner.jpeg',
     mediaDetails: { width: 1200, height: 600 },
-    altText: 'Portfolio Banner',
+    altText: 'Blog Banner',
   };
 
   return (
